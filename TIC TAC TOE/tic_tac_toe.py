3ae89face580b865286f26d3fc734f00ac2bbdb9
DHARNEESH S L <dharaneesh19112006@gmail.com>
2025-07-28 22:25:08 +0530

    Update tic_tac_toe.py

--- TIC TAC TOE/tic_tac_toe.py
+++ TIC TAC TOE/tic_tac_toe.py
@@ -6,15 +6,12 @@ def print_board(board):
 
 
 def check_win(board, player):
-    # Check rows
     for row in board:
         if all([cell == player for cell in row]):
             return True
-    # Check columns
     for col in range(3):
         if all([board[row][col] == player for row in range(3)]):
             return True
-    # Check diagonals
     if all([board[i][i] == player for i in range(3)]):
         return True
     if all([board[i][2 - i] == player for i in range(3)]):
